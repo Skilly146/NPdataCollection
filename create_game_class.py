d0253054f12069069718b432bf3af06086055387
Skilly146 <third.landon.davidson4@gmail.com>
2022-12-20 19:08:43 -0800

    Change: create_game class to create_player

--- create_game_class.py
+++ create_game_class.py
@@ -1,37 +1,49 @@
-import io
 import json
 import requests
 import os
 
 
-class create_game:
-    def __init__(self, game_number):
+# The term player refers to an api_key, NOT the actual player who I have no way of knowing.
+# It is impossible for the same player to be in multiple games since a player is a single instance
+class create_player:
+    # Takes a game number as input and adds the game to active_players.json to be used in the main loop
+    def __init__(self, api_key, game_number):
         self.game_number = str(game_number)
-        with open('registered_players.json', 'r') as f:
-            reg_players = json.load(f)
-        try:
-            self.all_players = reg_players[game_number]
-        except KeyError:
-            self.all_players = []
-            with open('registered_players.json', 'w') as f:
-                reg_players[game_number] = []
-                json.dump(reg_players, f, indent=2)
-
-        self.api_key = None
-        root = "http://nptriton.cqproject.net/game/{}/full".format(game_number)
-        self.payload = requests.post(root).json()
-        self.turn_based = self.payload['turn_based']
-
-    def add_player(self, api_key):
-        if api_key not in self.all_players:
-            with open('registered_players.json', 'r') as f:
-                reg_players = json.load(f)
-            with open('registered_players.json', 'w') as f:
-                reg_players[self.game_number].append(api_key)
-                json.dump(reg_players, f, indent=2)
         self.api_key = api_key
+
+        # checks if api_key is already in active_players.json. If yes it saves registered api_keys to
+        # self.all_players and if no (if KeyError) it adds empty register for the game number
+        with open('active_players.json', 'r') as f:
+            all_players = json.load(f)
+        try:
+            all_players[api_key]
+        except KeyError:
+            with open('active_players.json', 'w') as f:
+                all_players[api_key] = game_number
+                json.dump(all_players, f, indent=2)
+
+        self.game_location = 'database/' + self.game_number
+        self.player_location = self.game_location + '/' + api_key
+        # Checks if the player and game folders exists and if not creates them
+        if not os.path.exists(self.game_location):
+            os.mkdir(self.game_location)
+        if not os.path.exists(self.player_location):
+            os.mkdir(self.player_location)
+        # Initializes a blank settings.json file in player_location if one doesn't exist
+        if not os.path.exists(self.player_location + '/settings.json'):
+            with open(self.player_location + '/settings.json') as f:
+                json.dump({}, f, indent=2)
+
+        self.payload = None
         self.get_payload()
 
+    # Change the settings.json file in the players folder
+    def change_settings(self, settings):
+        # Creates settings.json which stores blocked_keys for stored payloads
+        with open(self.player_location + '/settings.json', 'w') as f:
+            json.dump(settings, f, indent=2)
+
+    # Gets payload from requested tick for whatever player's api key is in self.api_key
     def get_payload(self, tick=-1):
         if tick == -1:
             api_version = '0.1'
@@ -47,8 +59,19 @@ class create_game:
         return self.payload
 
     def save_payload(self):
-        payload = self.payload
-        tick = str(payload['tick'])
+        tick = str(self.payload['tick'])
+
+        payload = self.remove_blocked_keys()
+
+        tick_location = self.player_location + '/' + tick + '.json'
+        with open(tick_location, 'w') as f:
+            json.dump(payload, f, indent=2)
+
+    def remove_blocked_keys(self):
+
+        with open(self.player_location + '/settings.json', 'r') as f:
+            blocked_keys = json.load(f)['blocked_keys']
+        payload = {x: self.payload[x] for x in self.payload if x not in blocked_keys}
 
         all_keys = ['fleets', 'fleet_speed', 'paused', 'productions', 'tick_fragment',
                         'now', 'tick_rate', 'production_rate', 'stars', 'stars_for_victory',
@@ -57,16 +80,6 @@ class create_game:
                         'player_uid', 'admin', 'turn_based', 'war', 'players',
                         'turn_based_time_out']
         missing_keys = [key for key in all_keys if key not in payload]
-        payload['removed_keys'] = missing_keys
+        payload['blocked_keys'] = missing_keys
 
-        game_location = 'database/' + str(self.game_number)
-        player_location = game_location + '/' + self.api_key
-        tick_location = player_location + '/' + tick + '.json'
-        try:
-            os.mkdir(player_location)
-        except FileNotFoundError:
-            os.mkdir(game_location)
-            os.mkdir(player_location)
-        except FileExistsError:
-            with open(tick_location, 'w') as f:
-                json.dump(payload, f, indent=2)
+        return payload
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from create_game_class import create_game
+from create_game_class import create_player
 
 
-game_list = [create_game(5501867125374976)]
-game_list[0].add_player('347luk')
-# game_list[0].save_payload()
+settings = {'blocked_keys': ['stars', 'players']}
+player_list = [create_player('347luk', 5501867125374976)]
+player_list[0].change_settings(settings)
+player_list[0].save_payload()
