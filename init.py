a76add9f385abc7b280f83d19ae053bb81caf675
Skilly146 <third.landon.davidson4@gmail.com>
2022-12-21 00:40:42 -0800

    Add: finished init.py

--- init.py
+++ init.py
@@ -4,3 +4,5 @@ import os
 if not os.path.exists('active_players.json'):
     with open('active_players.json', 'x') as f:
         json.dump({}, f)
+if not os.path.exists('database/'):
+    os.mkdir('database')
